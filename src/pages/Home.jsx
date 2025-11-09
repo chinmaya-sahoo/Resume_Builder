170051abda85e6101adfe560b3bd953820a3f314
chinmaya-sahoo <chinmaysahoo668@gmail.com>
2025-11-09 21:48:34 +0530

    home file updated

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from '../commponents/home/Banner'
+import Hero from '../commponents/home/Hero'
+import Features from '../commponents/home/Features'
+
+function Home() {
+  return (
+    <div>
+      <Banner/>
+      <Hero/>
+      <Features/>
+    </div>
+  )
+}
+
+export default Home
