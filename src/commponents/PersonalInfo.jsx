174b01ca40923fd7ce470cf1e045b6cedf223db6
chinmaya-sahoo <chinmaysahoo668@gmail.com>
2025-11-14 12:58:00 +0530

    form-1 added

---
+++ src/commponents/PersonalInfo.jsx
@@ -0,0 +1,70 @@
+import { User } from "lucide-react";
+import React from "react";
+
+const PersonalInfo = ({
+  data,
+  onChange,
+  removeBackground,
+  setRemoveBackground,
+}) => {
+  const handleChange = (field, value) => {
+    onChange({ ...data, [field]: value });
+  };
+  return (
+    <div>
+      <h3 className="text-lg font-semibold text-gray-900">
+        Personal Information
+      </h3>
+      <p className="text-sm text-gray-600">
+        Get Started with the personal information
+      </p>
+      <div className="flex items-center gap-2">
+        <label>
+          {data.image ? (
+            <img
+              src={
+                typeof data.image === "string"
+                  ? data.image
+                  : URL.createObjectURL(data.image)
+              }
+              alt="user-image"
+              className="w-16 h-16 rounded-full object-cover mt-5 ring ring-slate-300 hover:opacity-80"
+            />
+          ) : (
+            <div className="flex flex-col items-center gap-2 mt-5 text-slate-500 hover:text-slate-700 cursor-pointer">
+              <User className="size-10 p-2.5 border rounded-full" />
+              <span className="text-xs text-gray-500">upload user image</span>
+            </div>
+          )}
+
+          <input
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={(e) => handleChange("image", e.target.files[0])}
+          />
+        </label>
+
+        {typeof data.image == "object" && (
+          <div className="flex flex-col gap-1 pl-4 text-sm">
+            <p>Remove Background</p>
+            <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
+              <input
+                type="checkbox"
+                className="sr-only peer"
+                onChange={() => setRemoveBackground((prev) => !prev)}
+                checked={removeBackground}
+              />
+              <div className="relative w-9 h-5 bg-slate-300 rounded-full peer peer-checked:bg-green-600 transition-colors duration-100">
+              </div>
+                <span className="dot absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-4"></span>
+            </label>
+          </div>
+        )}
+
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInfo;
