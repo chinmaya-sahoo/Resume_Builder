f29cddc3c3870a3779afe55abc0fe8b14febe4e6
chinmaya-sahoo <chinmaysahoo668@gmail.com>
2025-11-22 20:00:41 +0530

    new section added

--- src/commponents/EducationForm.jsx
+++ src/commponents/EducationForm.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const EducationForm = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+const EducationForm = ({ data, onChnage }) => {
+  const addExperience = () => {
+    const newExperience = {
+      company: "",
+      position: "",
+      start_date: "",
+      end_date: "",
+      description: "",
+      is_current: false,
+    };
+    onChange([...data, newExperience]);
+  };
 
-export default EducationForm
+  const removeExperience = (index) => {
+    const updated = data.filter((_, i) => i !== index);
+    onChange(updated);
+  };
+
+  const updateExperience = (index, field, value) => {
+    const updated = [...data];
+    updated[index] = { ...updated[index], [field]: value };
+    onChange(updated);
+  };
+  return <div></div>;
+};
+
+export default EducationForm;
