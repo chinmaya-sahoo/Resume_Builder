1f0bc29a03054e2e3070fc5026eb8045e7f5c373
chinmaya-sahoo <chinmaysahoo668@gmail.com>
2025-11-22 20:10:47 +0530

    new section updated

--- src/commponents/EducationForm.jsx
+++ src/commponents/EducationForm.jsx
@@ -1,29 +1,151 @@
+import { Plus, Sparkles, Trash2 } from "lucide-react";
 import React from "react";
 
-const EducationForm = ({ data, onChnage }) => {
-  const addExperience = () => {
-    const newExperience = {
-      company: "",
-      position: "",
-      start_date: "",
-      end_date: "",
-      description: "",
-      is_current: false,
+const EducationForm = ({ data, onChange }) => {
+  const addEducation = () => {
+    const newEducation = {
+      institution: "",
+      degree: "",
+      field: "",
+      graduation_date: "",
+      gpa: "",
     };
-    onChange([...data, newExperience]);
+    onChange([...data, newEducation]);
   };
 
-  const removeExperience = (index) => {
+  const removeEducation = (index) => {
     const updated = data.filter((_, i) => i !== index);
     onChange(updated);
   };
 
-  const updateExperience = (index, field, value) => {
+  const updateEducation = (index, field, value) => {
     const updated = [...data];
     updated[index] = { ...updated[index], [field]: value };
     onChange(updated);
   };
-  return <div></div>;
+  return (
+    <div className="space-y-6">
+      <div className="flex items-center justify-between">
+        <div>
+          <h3 className="flex items-center gap-2 text-lg font-semibold text-gray-900">
+            Professional Education
+          </h3>
+          <p className="text-sm text-gray-500">Add your job expperience here</p>
+        </div>
+        <button
+          onClick={addEducation}
+          className="flex items-center gap-2 px-3 py-2 text-sm bg-green-50 text-green-600 hover:bg-green-200 rounded-lg  transition-colors "
+        >
+          <Plus className="size-4" />
+          Add Education
+        </button>
+      </div>
+
+      {data.length === 0 ? (
+        <div className="text-center py-8 text-gray-500">
+          <Briefcase className="w-12 h-12 mx-auto mb-3 text-gray-300" />
+          <p>No Education added yet.</p>
+          <p className="text-sm">Click "Add Education" to get started.</p>
+        </div>
+      ) : (
+        <div className="space-y-4">
+          {data.map((Education, index) => (
+            <div
+              key={index}
+              className="p-4 border border-gray-200 rounded-lg space-y-3"
+            >
+              <div className="flex justify-between items-start">
+                <h4>Education #{index + 1}</h4>
+                <button
+                  onClick={() => removeEducation(index)}
+                  className="text-red-500 hover:text-red-700 transition-colors"
+                >
+                  <Trash2 className="size-4" />
+                </button>
+              </div>
+              <div className="grid md:grid-cols-2 gap-3">
+                <input
+                  value={Education.company || ""}
+                  onChange={(e) =>
+                    updateEducation(index, "company", e.target.value)
+                  }
+                  type="text"
+                  placeholder="Company Name"
+                  className="px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors"
+                />
+                <input
+                  value={Education.position || ""}
+                  onChange={(e) =>
+                    updateEducation(index, "position", e.target.value)
+                  }
+                  type="text"
+                  placeholder="Job Title"
+                  className="px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors"
+                />
+                <input
+                  value={Education.start_date || ""}
+                  onChange={(e) =>
+                    updateEducation(index, "start_date", e.target.value)
+                  }
+                  type="month"
+                  className="px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors"
+                />
+                <input
+                  value={Education.end_date || ""}
+                  onChange={(e) =>
+                    updateEducation(index, "end_date", e.target.value)
+                  }
+                  type="month"
+                  disabled={Education.is_current}
+                  className="px-3 py-2 text-sm border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors disabled:bg-gray-100"
+                />
+              </div>
+              <label className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  checked={Education.is_current || false}
+                  onChange={(e) => {
+                    updateEducation(
+                      index,
+                      "is_current",
+                      e.target.checked ? true : false
+                    );
+                  }}
+                  className="rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+                />
+                <span className="text-sm text-gray-700">
+                  Currently working here
+                </span>
+              </label>
+
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <label className="text-sm font-medium text-gray-700">
+                    Job Description
+                  </label>
+
+                  <button className="flex items-center gap-1 px-2 py-1 text-xs bg-purple-100 text-purple-700 rounded hover:bg-purple-200 transition-colors disabled:opacity-50">
+                    <Sparkles className="w-3 h-3" />
+                    Enhance with AI
+                  </button>
+                </div>
+
+                <textarea
+                  value={Education.description || ""}
+                  onChange={(e) =>
+                    updateEducation(index, "description", e.target.value)
+                  }
+                  rows={4}
+                  className="w-full text-sm px-3 py-2 rounded-lg resize-none border border-gray-300 focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors"
+                  placeholder="Describe your key responsibilities and achievements..."
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default EducationForm;
