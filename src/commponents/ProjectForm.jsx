c9231364b019ebbcdc70aa8f5b729b30193b7646
chinmaya-sahoo <chinmaysahoo668@gmail.com>
2025-11-29 20:38:07 +0530

    project form updated - 1.1

--- src/commponents/ProjectForm.jsx
+++ src/commponents/ProjectForm.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 
 const ProjectForm = ({data , onChange}) => {
+    const addProject = () => {
+    const newProject = {
+      institution: "",
+      degree: "",
+      field: "",
+      graduation_date: "",
+      gpa: "",
+    };
+    onChange([...data, newProject]);
+  };
+
+  const removeProject = (index) => {
+    const updated = data.filter((_, i) => i !== index);
+    onChange(updated);
+  };
+
+  const updateProject = (index, field, value) => {
+    const updated = [...data];
+    updated[index] = { ...updated[index], [field]: value };
+    onChange(updated);
+  };
   return (
     <div>
         
